3c44f317e12a6d693b88f8bce6fe4ab11ca984f2
Sushma <113226796+SushMallesh@users.noreply.github.com>
2022-10-31 15:00:59 +0530

    updated code with different approach

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,40 @@
+import {Component} from 'react'
 import Message from '../Message'
+import Login from '../Login'
+import Logout from '../Logout'
+
 import './index.css'
 
-const Home = () => (
-  <div className="app-container">
-    <Message />
-  </div>
-)
+class Home extends Component {
+  state = {isLoggedIn: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+  }
+
+  render() {
+    const {isLoggedIn} = this.state
+    return (
+      <div className="app-container">
+        <div className="message-container">
+          <Message isLoggedIn={isLoggedIn} />
+          {!isLoggedIn && <Login login={this.onClickButton} />}
+          {isLoggedIn && <Logout logout={this.onClickButton} />}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+// import Message from '../Message'
+// import './index.css'
+
+// const Home = () => (
+//   <div className="app-container">
+//     <Message />
+//   </div>
+// )
 
 export default Home
