1c96688b6fcfb4f8c8d2a7d4ac95145ff3f6de81
Sushma <113226796+SushMallesh@users.noreply.github.com>
2022-10-31 14:57:16 +0530

    updated code

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,35 +1,48 @@
-import {Component} from 'react'
-import Login from '../Login'
-import Logout from '../Logout'
+
+
 import './index.css'
 
-class Message extends Component {
-  state = {isLoggedIn: false}
-
-  onLogin = () => {
-    this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
-  }
-
-  onLogout = () => {
-    this.setState(prevState => ({isLoggedIn: prevState.isLoggedOut}))
-  }
-
-  message = () => {
-    const {isLoggedIn} = this.state
-    return isLoggedIn ? 'Welcome User' : 'Please Login'
-  }
-
-  render() {
-    const {isLoggedIn} = this.state
-    const message = this.message()
-    return (
-      <div className="message-container">
-        <h1 className="message">{message}</h1>
-        {!isLoggedIn && <Login onLogin={this.onLogin} />}
-        {isLoggedIn && <Logout onLogout={this.onLogout} />}
-      </div>
-    )
-  }
+const Message = props => {
+  const {isLoggedIn} = props
+  const message = isLoggedIn ? 'Welcome User' : 'Please Login'
+  return <h1 className="message">{message}</h1>
 }
+
+
+
+
+// import {Component} from 'react'
+// import Login from '../Login'
+// import Logout from '../Logout'
+// import './index.css'
+
+// class Message extends Component {
+//   state = {isLoggedIn: false}
+
+//   onLogin = () => {
+//     this.setState(prevState => ({isLoggedIn: !prevState.isLoggedIn}))
+//   }
+
+//   onLogout = () => {
+//     this.setState(prevState => ({isLoggedIn: prevState.isLoggedOut}))
+//   }
+
+//   message = () => {
+//     const {isLoggedIn} = this.state
+//     return isLoggedIn ? 'Welcome User' : 'Please Login'
+//   }
+
+//   render() {
+//     const {isLoggedIn} = this.state
+//     const message = this.message()
+//     return (
+//       <div className="message-container">
+//         <h1 className="message">{message}</h1>
+//         {!isLoggedIn && <Login onLogin={this.onLogin} />}
+//         {isLoggedIn && <Logout onLogout={this.onLogout} />}
+//       </div>
+//     )
+//   }
+// }
 
 export default Message
