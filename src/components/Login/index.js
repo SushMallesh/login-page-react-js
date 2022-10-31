fc1ad9ba811339c69502dd8150e40aa4fb61855f
Sushma <113226796+SushMallesh@users.noreply.github.com>
2022-10-31 15:00:04 +0530

    updated code with different approach

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,16 +1,30 @@
 import './index.css'
 
 const Login = props => {
-  const {onLogin} = props
-
-  const onClickLogin = () => {
-    onLogin()
-  }
+  const {login} = props
 
   return (
-    <button onClick={onClickLogin} className="button" type="button">
+    <button type="button" className="login-button" onClick={login}>
       Login
     </button>
   )
 }
+
+
+
+// import './index.css'
+
+// const Login = props => {
+//   const {onLogin} = props
+
+//   const onClickLogin = () => {
+//     onLogin()
+//   }
+
+//   return (
+//     <button onClick={onClickLogin} className="button" type="button">
+//       Login
+//     </button>
+//   )
+// }
 export default Login
